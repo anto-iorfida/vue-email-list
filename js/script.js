6d3de8840bf314fe68aa4ea57523554333060fc1
anto-iorfida <antonioiorfida@hotmail.com>
2024-04-09 15:56:45 +0200

    feat create function generate email with api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
-// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.Bonus
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            // array empty to fill
+            mailGenerate:[],
+            
+        };
+    },
+    methods: {
+        // function email generate api
+        getGenerateEmailApi(){
+                        // api generate mail 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // single email generate
+                this.dataResponse = response.data.response,
+                // push in array mailGenerate single email generate
+                this.mailGenerate.push(this.dataResponse);
+            });
+        }
+        
+    },
+    mounted(){
+        
+    }
+}).mount('#app');
